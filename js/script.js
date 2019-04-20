152269aa670c8fd1efe58e98c0434bed0a7760af
AayushDongre <aayushdongre4@gmail.com>
2019-04-21 05:28:28 +0530

    Made everything responsive and cleaned some code

--- js/script.js
+++ js/script.js
@@ -16,39 +16,32 @@ document.addEventListener("wheel", function (e) {
     var currenttime = new Date().getTime();
      var difference = currenttime - prevtime;
 
-    console.log(e.deltaY + " " + difference + " " + back);
+    // console.log(e.deltaY + " " + difference + " " + back);
     
-    if(e.deltaY>=0 && difference > 1300 && navopen==false)//DOWN
+    //MOUSE SCROLLED DOWN
+    if(e.deltaY>=0 && difference > 2700 && navopen==false)
     {
              i=i+1;
              if(i==3)
               i = 0;
         back =  i + ".jpg";
-    //  Change counter color
+        //CHANGE COUNTER COLOR AND COUNT
         $(".counter .page").text(i+1);
         var color = $("#p"+(i+1)+" .title").css("color");
         $(".page").css("color",color);
-
+        //CHANGE IMGBOX IMAGE
         $(".imgbox").animate({width:0,left:"81.45%"},650).queue(function(){
             $(this).css({background:"url(img/"+back+")","background-size":"cover"});
             $(this).dequeue();
         }).delay(500).animate({width:"20.57%",left:"60.88%"},650);
-
+        //ERASE CURRENT ELEMENTS
         current.children("div.title").delay(800).animate({top:"-50px",opacity:"0"});
-        
         current.children("div.infobox").children("div.head").children("h2.info-title").delay(600).animate({top:"-50px",opacity:"0"});
-        
-         current.children("div.infobox").children("div.head").children("p.info-des").delay(500).animate({top:"-50px",opacity:"0"});
-        
+        current.children("div.infobox").children("div.head").children("p.info-des").delay(500).animate({top:"-50px",opacity:"0"});
         current.children("div.infobox").children("div.description").children("p.what-des").delay(600).animate({top:"-50px",opacity:"0"});
-        
-         //current.children("div.infobox").children("div.description").children("p.what-des").css({animation:"text-out-up .65s cubic-bezier(0, 0, .03, 1.42) .5s forwards"});
-
          
-
         current.delay(700).animate({opacity:0},500);
-        
-        
+        //CHECK IF LAST ELEMENT
         if(current.attr("id")!="p3"){
             current = current.next();
             if(current.attr("class")=="imgbox")
@@ -58,54 +51,44 @@ document.addEventListener("wheel", function (e) {
             current = $("#p1");
         }
        
-        
         current.delay(1400).animate({opacity:1},500);
         
-        
-    current.children("div.title").delay(2000).css({opacity:"0",top:"100%"}).animate({top:"35.29%",opacity:"1"});
-        
+        //SPAWN CURRENT ELEMENTS
+        current.children("div.title").delay(2000).css({opacity:"0",top:"100%"}).animate({top:"35.29%",opacity:"1"});
         current.children("div.infobox").children("div.head").children("h2.info-title").delay(1700).css({opacity:"0",top:"100%"}).animate({top:"",opacity:"1"});
-        
-         current.children("div.infobox").children("div.head").children("p.info-des").delay(1600).css({opacity:"0",top:"100%"}).animate({top:"",opacity:"1"}); 
-        
-         current.children("div.infobox").children("div.description").children("p.what-des").delay(1600).css({opacity:"0",top:"100%"}).animate({top:"",opacity:"1"});
+        current.children("div.infobox").children("div.head").children("p.info-des").delay(1600).css({opacity:"0",top:"100%"}).animate({top:"",opacity:"1"}); 
+        current.children("div.infobox").children("div.description").children("p.what-des").delay(1600).css({opacity:"0",top:"100%"}).animate({top:"",opacity:"1"});
         
         prevtime = currenttime;
-    }
-    
-    
-    else if(e.deltaY<=0  && difference > 1300 && navopen==false)//UP
+    }//SCROLL DOWN END
+
+    //MOUSE SCROLLED UP
+    else if(e.deltaY<=0  && difference > 2700 && navopen==false)
     {
         i=i-1;
         if(i==-1)
         i = 2;
         back =  i + ".jpg";
 
-        //  Change counter color
+        //CHANGE COUNTER COLOR AND COUNT
         $(".counter .page").text(i+1);
         var color = $("#p"+(i+1)+" .title").css("color");
         $(".page").css("color",color);
 
-
-
-      $(".imgbox").animate({width:0,left:"81.45%"},650).queue(function(){
+        //CHANGE IMGBOX IMAGE
+        $(".imgbox").animate({width:0,left:"81.45%"},650).queue(function(){
             $(this).css({background:"url(img/"+back+")","background-size":"cover"});
             $(this).dequeue();
         }).delay(500).animate({width:"20.57%",left:"60.88%"},650);
 
-        
+        //ERASE CURRENT ELEMENTS
         current.children("div.title").delay(800).animate({opacity:"0",top:"100%"});
-        
         current.children("div.infobox").children("div.head").children("h2.info-title").delay(600).animate({opacity:"0",top:"50px"});
-        
-         current.children("div.infobox").children("div.head").children("p.info-des").delay(500).animate({opacity:"0",top:"50px"}); 
-        
-         current.children("div.infobox").children("div.description").children("p.what-des").delay(600).animate({opacity:"0",top:"50px"});
-
+        current.children("div.infobox").children("div.head").children("p.info-des").delay(500).animate({opacity:"0",top:"50px"}); 
+        current.children("div.infobox").children("div.description").children("p.what-des").delay(600).animate({opacity:"0",top:"50px"});
         current.delay(700).animate({opacity:0},500);
         
-         
-        
+        //CHECK IF FIRST ELEMENT        
          if(current.attr("id")!="p1"){
             current = current.prev();
             if(current.attr("class")=="imgbox")
@@ -115,25 +98,104 @@ document.addEventListener("wheel", function (e) {
             current = $("#p3");
         }      
         
-        
-        
         current.delay(1400).animate({opacity:1},500);
         
-        
+        //SPAWN CURRENT ELEMENTS
         current.children("div.title").delay(2000).css({opacity:"0",top:"-50px"}).animate({top:"35.29%",opacity:"1"});
-        
         current.children("div.infobox").children("div.head").children("h2.info-title").delay(1700).css({opacity:"0",top:"-50px"}).animate({top:"",opacity:"1"});
-        
-         current.children("div.infobox").children("div.head").children("p.info-des").delay(1600).css({opacity:"0",top:"-50px"}).animate({top:"",opacity:"1"}); 
-        
-         current.children("div.infobox").children("div.description").children("p.what-des").delay(1600).css({opacity:"0",top:"-50px"}).animate({top:"",opacity:"1"});
+        current.children("div.infobox").children("div.head").children("p.info-des").delay(1600).css({opacity:"0",top:"-50px"}).animate({top:"",opacity:"1"}); 
+        current.children("div.infobox").children("div.description").children("p.what-des").delay(1600).css({opacity:"0",top:"-50px"}).animate({top:"",opacity:"1"});
 
-      //   $(".imgbox").css({animation:"img-in  1s cubic-bezier(0, 0, .03, 1.42) 1.5s forwards"});
+        prevtime = currenttime;
+    }//SCROLL UP END
+    
+},true);//SCROLL ANIMATION END
+
+//ARROW CLICK ANIMATION
+
+//IF RIGHT ARROW PRESSED
+$(".rightarrow").click(function(){
+    var currenttime = new Date().getTime();
+    var difference = currenttime - prevtime;
+    if(difference >= 2700)
+    {
+        i=i+1;
+        if(i==3)
+        i = 0;
+        
+
+        //CHANGE COUNTER COLOR AND COUNT
+        $(".counter .page").text(i+1);
+        var color = $("#p"+(i+1)+" .title").css("color");
+        $(".page").css("color",color);
+        //ERASE CURRENT ELEMENTS
+        current.children("div.title").delay(800).animate({top:"-50px",opacity:"0"});
+        current.children("div.infobox").children("div.head").children("h2.info-title").delay(600).animate({top:"-50px",opacity:"0"});
+        current.children("div.infobox").children("div.head").children("p.info-des").delay(500).animate({top:"-50px",opacity:"0"});
+        current.children("div.infobox").children("div.description").children("p.what-des").delay(600).animate({top:"-50px",opacity:"0"});
+    
+        current.delay(700).animate({opacity:0},500);
+        //CHECK IF LAST ELEMENT
+        if(current.attr("id")!="p3"){
+        current = current.next();
+        if(current.attr("class")=="imgbox")
+            current=current.next();
+        }
+        else if(current.attr("id")=="p3"){
+        current = $("#p1");
+        }
+
+        current.delay(1400).animate({opacity:1},500);
+        //SPAWN CURRENT ELEMENTS
+        current.children("div.title").delay(2000).css({opacity:"0",top:"100%"}).animate({top:"6vh",opacity:"1"});
+        current.children("div.infobox").children("div.head").children("h2.info-title").delay(1700).css({opacity:"0",top:"100%"}).animate({top:"",opacity:"1"});
+        current.children("div.infobox").children("div.head").children("p.info-des").delay(1600).css({opacity:"0",top:"100%"}).animate({top:"",opacity:"1"}); 
+        current.children("div.infobox").children("div.description").children("p.what-des").delay(1600).css({opacity:"0",top:"100%"}).animate({top:"",opacity:"1"});
+
         prevtime = currenttime;
     }
-    
-},true);
-    
+});//RIGHT ARROW CLICK END
+
+//IF LEFT ARROW PRESSED
+$(".leftarrow").click(function(){
+    var currenttime = new Date().getTime();
+    var difference = currenttime - prevtime;
+    if(difference >= 2700)
+    {
+        i=i-1;
+        if(i==-1)
+        i = 2;
+        //CHANGE COUNTER COLOR AND COUNT
+        $(".counter .page").text(i+1);
+        var color = $("#p"+(i+1)+" .title").css("color");
+        $(".page").css("color",color);
+        //ERASE CURRENT ELEMENTS    
+        current.children("div.title").delay(800).animate({opacity:"0",top:"100%"});
+        current.children("div.infobox").children("div.head").children("h2.info-title").delay(600).animate({opacity:"0",top:"50px"});
+        current.children("div.infobox").children("div.head").children("p.info-des").delay(500).animate({opacity:"0",top:"50px"}); 
+        current.children("div.infobox").children("div.description").children("p.what-des").delay(600).animate({opacity:"0",top:"50px"});
+        current.delay(700).animate({opacity:0},500);
+        //CHECK IF FIRST ELEMENT
+        if(current.attr("id")!="p1"){
+            current = current.prev();
+            if(current.attr("class")=="imgbox")
+                current=current.prev();
+        }
+        else if(current.attr("id")=="p1"){
+            current = $("#p3");
+        }      
+        //SPAWN CURRENT ELEMENTS    
+        current.delay(1400).animate({opacity:1},500);
+        current.children("div.title").delay(2000).css({opacity:"0",top:"-50px"}).animate({top:"6vh",opacity:"1"});
+        current.children("div.infobox").children("div.head").children("h2.info-title").delay(1700).css({opacity:"0",top:"-50px"}).animate({top:"",opacity:"1"});
+        current.children("div.infobox").children("div.head").children("p.info-des").delay(1600).css({opacity:"0",top:"-50px"}).animate({top:"",opacity:"1"}); 
+        current.children("div.infobox").children("div.description").children("p.what-des").delay(1600).css({opacity:"0",top:"-50px"}).animate({top:"",opacity:"1"});
+        
+        prevtime = currenttime;
+    } 
+});//LEFT ARROW CLICK END
+
+
 //INFOBOX MOUSE ANIMATION
 var card = $(".infobox");
 card.on("mousemove",function(e) {  
@@ -143,14 +205,10 @@ card.on("mousemove",function(e) {
     card.css({ transform: "rotateY("+ax+"deg) rotateX("+ay+"deg)","-webkit-transform": "rotateY("+ax+"deg) rotateX("+ay+"deg)","-moz-transform": "rotateY("+ax+"deg) rotateX("+ay+"deg)"});
 });
 card.on("mouseleave",function(e) {  
-    
-
     card.css({ transform: "rotateY("+0+"deg) rotateX("+0+"deg)","-webkit-transform": "rotateY("+0+"deg) rotateX("+0+"deg)","-moz-transform": "rotateY("+0+"deg) rotateX("+0+"deg)"});
 });
     
-    
-console.log($(window).outerWidth() + " " + $(window).outerHeight()  );    
-    
+        
 var h = $(window).outerHeight();
 var w = $(window).outerWidth();
 $(".wrapper").css({height:h,width:w})
@@ -173,6 +231,7 @@ $(".info").css({height:.4162*h,width:.2916*w})
 
 //NAVBAR MENU    
 var navopen = false;
+    //IF OPEN BUTTON IS PRESSED
     $("#openmenu").click(function(){
        $("#navmain").fadeIn(200); 
         $("#openmenu").animate({left:"-20vw"});
@@ -183,7 +242,7 @@ var navopen = false;
         
         navopen = true;
     });
-    
+    //IF CLOSE BUTTON IS PRESSED
     $("#closemenu").click(function(){
        $("#navmain").fadeOut(200); 
         $("#openmenu").animate({left:"3%"});
@@ -197,8 +256,8 @@ var navopen = false;
         navopen = false;
     });
   
-    
-    $("#right ul li").click(function(){  
+//NAVMENU RIGHT WIDTH ANIMATION    
+$("#right ul li").click(function(){  
         $(".current").toggleClass("current");
         $(this).children(".menu").toggleClass("current");
         
@@ -213,7 +272,7 @@ var navopen = false;
                 currentl.fadeOut(200);
                 futurel.fadeIn(200).addClass("currentleft");
             } 
-    });   
+});   
 
     
 //PLUGIN INITIALIZATIONS    
